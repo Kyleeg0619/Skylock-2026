15753d68f63ac10694db7227f89d2bdfbe53000b
Kylee Grasela <Kyleeg0619@gmail.com>
2026-02-17 13:31:08 -0500

    Clear Firebase configuration values
    Removed Firebase configuration values for security reasons.

--- SkyLock/src/firebase.js
+++ SkyLock/src/firebase.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBLcibdWWfUj7viJqm1UbBe2etp2JYBhDM",
-  authDomain: "skylock-c920c.firebaseapp.com",
-  databaseURL: "https://skylock-c920c-default-rtdb.firebaseio.com",
-  projectId: "skylock-c920c",
-  storageBucket: "skylock-c920c.firebasestorage.app",
-  messagingSenderId: "1063832687502",
-  appId: "1:1063832687502:web:f3dce9cd24373703e68adf",
-  measurementId: "G-6C30D1BBCE"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
